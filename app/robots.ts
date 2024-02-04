4a36efeaac44eb9055aff4040c8a416031d3bf1b
kinhbach242 <kinhbach.dev@gmail.com>
2024-02-04 17:42:05 +0700

    change metadata

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,10 @@ export default function robots() {
   return {
     rules: [
       {
-        userAgent: '*',
+        userAgent: "*",
       },
     ],
-    sitemap: 'https://leerob.io/sitemap.xml',
-    host: 'https://leerob.io',
-  };
+    sitemap: "https://kinhdev.id.vn/sitemap.xml",
+    host: "https://kinhdev.id.vn",
+  }
 }
